babe53a66d89ad73e4ef9e1c9bc11722aadfebab
neshwa <neshwaparveenn@gmail.com>
2026-01-19 01:01:15 +0530

    Initial Commit

---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by Neshwa on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
